e38f7ac2d72643be8fc9c866f6931275cdbcf01b
carlosrodriguesjf <carlosrodriguesjf@gmail.com>
2023-11-23 21:45:30 -0300

    atualizacao 23/11/23

--- analise-sono.R
+++ analise-sono.R
@@ -5,12 +5,15 @@ getwd()
 # Instalando os pacotes
 install.packages("readr")
 install.packages("dplyr")
+install.packages("ggplot2")
 library(readr)
 library(dplyr)
-
+library(ggplot2)
 
 # Carregando o dataset
 df_sono = read_csv("dados/sono.csv")
+
+dim(df_sono)
 
 # Visualizando os dados em formato de tabela
 View(df_sono)
@@ -18,18 +21,67 @@ View(df_sono)
 # visualizando parte dos dados
 head(df_sono)
 
-# classe do dataframe
-class(df_sono)
-
 # resumo dos tipos de dados da coluna
 str(df_sono)
+
+# Sumario das variáveis numericas
+summary(df_sono)
 
 # exibindo o nome das colunas
 print(colnames(df_sono))
 
+
+
+
+
+## ANÁLISE EXPLORATÓRIA ###
+
 # alterando o nome das colunas
-colnames(df_sono) <- c("id","idade","genero","horas_sono","problemas_sono","cafeina","exercicio","nome","peso","humor_ao_acordar")
+colnames(df_sono) <- c("id","nome","idade","genero","duracao_sono","habitos_pre_sono", "problemas_sono",
+                       "padrao_sono", "ambiente_sono", "cronotipo", "estresse", "cafeina","exercicio","peso","humor_ao_acordar")
 print(names)
+
+# Quantos linhas tem casos completos?
+complete_cases <- sum(complete.cases(df_sono))
+
+# Quantos linhas tem casos não completos?
+not_complete_cases <- sum(!complete.cases(df_sono))
+
+# percentual 
+percentual <- (not_complete_cases / complete_cases) * 100
+percentual
+
+# removendo os objetos anteriores para liberar memoria ram
+
+rm(complete_cases)
+rm(not_complete_cases)
+
+
+# verificando quais as idades presente
+idades <- unique(df_sono$idade)
+range(idades)
+rm(anos)
+
+# numero de registros por idade
+table(df_sono$idade)
+
+
+# extraindo variaeis numericas
+lista_variaveis_numericas <- sapply(df_sono, is.numeric)
+dados_numericos <- df_sono[lista_variaveis_numericas]
+
+
+# matriz de correlação
+cor(dados_numericos)
+
+# plot correlação
+pairs(dados_numericos)
+pairs(dados_numericos[1:3],labels = colnames(dados_numericos)[1:3])
+pairs(dados_numericos[4:6],labels = colnames(dados_numericos)[4:6])
+
+
+
+
 
 # contagem e histograma
 count(df_sono, genero)
